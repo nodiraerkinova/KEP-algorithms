ad85f4bc561b3389b186d9e6e5d510a4c570d53a
nodiraerkinova <nodiraerkinova2010@gmail.com>
2026-04-28 16:59:02 +0500

    377 algo

--- 377.py
+++ 377.py
@@ -1,4 +1,8 @@
 n = int(input())
 a = list(map(int, input().split()))
-b = set(a)
-print(b)
+counter = 0
+for element in a:
+    if a.count(element) == 2:
+        counter += 1
+
+print(int(counter / 2))
