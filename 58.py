07ee069d89bf040d9b5e1af93b64be39415728c1
nodiraerkinova <nodiraerkinova2010@gmail.com>
2026-05-05 17:20:47 +0500

    coment

--- 58.py
+++ 58.py
@@ -1,14 +1,13 @@
 def filter_list(lst, son):
+    new_lst = lst.copy()
     if son == 0:
-        for num in lst:
+        for num in new_lst:
             if num % 2 == 0:
                 lst.remove(num)
         return lst
     else:
-        for num in lst:
-            if num % 2 != 0:
+        for num in new_lst:
+            if num % 2 == 1:
                 lst.remove(num)
         return lst
-
-print(filter_list([1, 2, 3, 4, 5], 0))
-
+    
